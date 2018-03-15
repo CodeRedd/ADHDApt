61f4532712447b59a97c71c87ee7c2381b807e6e
Culver Ganem-Redd <culver@code-redd.com>
2018-03-15 19:50:48 -0400

    Basic implementation of IToolProvider

---
+++ app/src/main/java/com/coderedd/adhdapt/model/ToolLoader.java
@@ -0,0 +1,15 @@
+package com.coderedd.adhdapt.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by CodeRedd on 3/15/2018.
+ */
+
+public class ToolLoader implements IToolProvider
+{
+    public ArrayList<ITool> tools()
+    {
+        return new ArrayList<ITool>();
+    }
+}
---
+++ app/src/test/java/com/coderedd/adhdapt/model/ToolLoaderTest.java
@@ -0,0 +1,10 @@
+package com.coderedd.adhdapt.model;
+
+/**
+ * Created by CodeRedd on 3/15/2018.
+ */
+
+public class ToolLoaderTest
+{
+    
+}
