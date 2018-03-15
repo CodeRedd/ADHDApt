b64d103d4b0ea004f7399afd63b77230be9aa981
Culver Redd <culver@code-redd.com>
2018-03-15 15:02:31 -0400

    + new Android Studio project for app

---
+++ app/src/main/java/com/coderedd/adhdapt/ToolDisplayActivity.java
@@ -0,0 +1,13 @@
+package com.coderedd.adhdapt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToolDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_display);
+    }
+}
