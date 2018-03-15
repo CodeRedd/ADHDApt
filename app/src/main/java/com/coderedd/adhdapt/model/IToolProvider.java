9adf581c8b6e7cc29fd89470d7efe1b174c031cf
Culver Ganem-Redd <culver@code-redd.com>
2018-03-15 19:27:43 -0400

    + IToolProvider

---
+++ app/src/main/java/com/coderedd/adhdapt/model/IToolProvider.java
@@ -0,0 +1,12 @@
+package com.coderedd.adhdapt.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by CodeRedd on 3/15/2018.
+ */
+
+public interface IToolProvider
+{
+    ArrayList<ITool> tools();
+}
